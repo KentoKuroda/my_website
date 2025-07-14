e7d04aeef2680a5afdd2a4b7fe598b145f9d4dca
KurodaKento0505 <kuroda.kento@image.iit.tsukuba.ac.jp>
2025-07-15 00:10:26 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+      page.classList.remove('active');
+    });
+  
+    const target = document.getElementById(pageId);
+    if (target) {
+      target.classList.add('active');
+    }
+  }
+  
